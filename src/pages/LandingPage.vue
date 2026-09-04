e66f91f58052310f41e9da84ac8e25d5dfb4511a
Fitrah-nursidik <dikdiklonely667@gmail.com>
2026-09-04 07:54:05 +0700

    update mx-8

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -14,7 +14,7 @@
  Browser kamu tidak mendukung pemutaran video.
 </video>
     <!-- Bagian Fitur (Menggunakan Props) -->
-    <section class="inline-flex flex-wrap gap-[20px] justify-center p-[150px] mx-auto">
+    <section class="inline-flex flex-wrap gap-[20px] justify-center p-[150px] mx-8">
       <FeatureCard 
         icon="🏷️" 
         title="Kualitas Terjamin" 
