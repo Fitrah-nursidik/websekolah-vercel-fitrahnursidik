eedbb59dd09384dba69da456ac2eea8ab837147b
Fitrah-nursidik <dikdiklonely667@gmail.com>
2026-09-22 10:37:38 +0700

    update

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -14,7 +14,7 @@
  Browser kamu tidak mendukung pemutaran video.
 </video>
     <!-- Bagian Fitur (Menggunakan Props) -->
-    <section class="inline-flex flex-wrap gap-[20px] justify-center p-[150px] m-[<30px>]">
+    <section class="inline-flex flex-wrap gap-[20px] justify-center p-[0 20px 40px 20px] m-[<30px>]">
       <FeatureCard 
         icon="🏷️" 
         title="Kualitas Terjamin" 
